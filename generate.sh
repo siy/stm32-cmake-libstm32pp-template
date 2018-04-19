d0c7873c07337bd917b45534c9e35da4b4e70a5a
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2018-04-19 19:00:19 +0200

    Fixed generate.sh

--- generate.sh
+++ generate.sh
@@ -1,2 +1,9 @@
-cmake -DSTM32_CHIP=STM32F103C8 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc_stm32.cmake -DCMAKE_BUILD_TYPE=Debug cmake-build-debug
-cmake -DSTM32_CHIP=STM32F103C8 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc_stm32.cmake -DCMAKE_BUILD_TYPE=Release cmake-build-release
+mkdir cmake-build-debug
+cd cmake-build-debug
+cmake -DSTM32_CHIP=STM32F103C8 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc_stm32.cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
+cd ..
+
+mkdir cmake-build-release
+cd cmake-build-release
+cmake -DSTM32_CHIP=STM32F103C8 -DCMAKE_TOOLCHAIN_FILE=cmake/gcc_stm32.cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ./cmake-build-release/
+cd ..
